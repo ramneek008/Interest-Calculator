c7d554d34e7c2ef1c713dc2cc693b4456ca99a06
ramneek008 <singhramneek274@gmail.com>
2020-11-13 11:04:12 +0530

    calculator.java added

---
+++ src/Calculator.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class Calculator {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		int choice=0;
+
+		do{
+			System.out.println("MAIN MENU\n---------");
+			System.out.println("1. Interest Calculator - SB\n2. Interest Calculator - FD\n3. Interest Calculator - RD\n4. Exit");
+			System.out.println("Enter your option (1..4) : ");
+			choice = sc.nextInt();
+			switch(choice) {
+			case 1:
+			{
+				System.out.print("Enter the Average amount in your account: ");
+				System.out.println("Interest gained: ");
+			}
+			
+			case 2:
+			{
+				FDAccount fd = new FDAccount();
+				System.out.print("Enter the FD amount: ");
+				fd.setAmount(sc.nextDouble());;
+				System.out.print("Enter the no. of days: ");
+				fd.setNoOfDays(sc.nextInt());
+				System.out.print("Enter your age: ");
+				fd.setAgeOfACHolder(sc.nextInt());
+				System.out.println("Interest gained is: "+ fd.getInterestRate());
+			}
+			
+			case 3:
+			{
+				
+			}
+			
+			case 4:
+			{
+				System.out.println("Exited");
+				break;
+			}
+			
+			default:
+			{
+				System.out.println("Invalid input");
+				break;
+			}
+			
+			}
+		} while(choice!=4);
+		sc.close();
+	}
+
+}
--- src/FDAccount.java
+++ src/FDAccount.java
@@ -1,14 +1,48 @@
 
 public class FDAccount extends Account{
 
-	double interestRate;
-	double amount;
-	int noOfDays;
-	int ageOfACHolder;
+	private double interestRate;
+	private double amount;
+	private int noOfDays;
+	private int ageOfACHolder;
 	
+	public double getInterestRate() {
+		return interestRate;
+	}
+
+	public void setInterestRate(double interestRate) {
+		this.interestRate = interestRate;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public int getNoOfDays() {
+		return noOfDays;
+	}
+
+	public void setNoOfDays(int noOfDays) {
+		this.noOfDays = noOfDays;
+	}
+
+	public int getAgeOfACHolder() {
+		return ageOfACHolder;
+	}
+
+	public void setAgeOfACHolder(int ageOfACHolder) {
+		this.ageOfACHolder = ageOfACHolder;
+	}
+
 	@Override
 	double calculateInterest() {
 		// TODO Auto-generated method stub
+		
 		return 0;
 	}
+	
 }
